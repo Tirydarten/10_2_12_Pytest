2fdbb54edac178c37e4571699a0bcc15267df4a9
Artem Volkov <tirydarten@mail.ru>
2024-10-16 15:31:31 +0700

    changes func - taxes.py and tests - test_taxes.py according task 2

--- tests/test_taxes.py
+++ tests/test_taxes.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.taxes import calculate_taxes
+from src.taxes import calculate_taxes, calculate_tax
 
 
 @pytest.fixture()
@@ -21,3 +21,22 @@ def test_taxes_invalid_tax_rate(prices):
 def test_taxes_invalid_prices():
     with pytest.raises(ValueError):
         calculate_taxes([0, -1], tax_rate=10)
+
+@pytest.mark.parametrize("price, tax_rate, expected", [(50, 5, 52.5), (100, 10, 110)])
+
+
+def test_calculate_tax(price, tax_rate, expected):
+    assert calculate_tax(price, tax_rate) == expected
+
+def test_calculate_tax_invalid_price():
+    with pytest.raises(ValueError):
+        calculate_tax(-1, 10)
+
+
+def test_calculate_tax_invalid_tax_rate_below_zero():
+    with pytest.raises(ValueError):
+        calculate_tax(100, -1)
+
+def test_calculate_tax_invalid_tax_rate_after_100():
+    with pytest.raises(ValueError):
+        calculate_tax(100, 1000)
